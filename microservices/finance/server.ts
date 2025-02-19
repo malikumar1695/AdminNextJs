7d2a34bf77dafff1dbabd9cac665afa6ed644553
Umer Sultan <malikumar1695@gmail.com>
2025-02-19 19:49:18 +0300

    servers setup for microservices

---
+++ microservices/finance/server.ts
@@ -0,0 +1,17 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app: Application = express();
+
+
+const port = process.env.PORT || 8001;
+
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+
+    res.send('here we go 8001');
+})
+
+app.listen(port, () => {
+    console.log('app is listening on ', 8001);
+})
---
+++ microservices/students/server.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response, NextFunction, Application } from 'express';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Application = express();
+
+const port = process.env.PORT || 8002;
+
+app.use('/', (req: Request, res: Response, next:NextFunction)=> {
+    res.send('here we go')
+})
+
+app.listen(port, () => {
+    console.log(`app is listening on ${port}`);
+})
