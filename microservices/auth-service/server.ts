4434ca7f36db5d76a25ff4c2cbf73209c8cf7094
Umer Sultan <malikumar1695@gmail.com>
2025-03-01 13:48:34 +0300

    added auth microservice and modified names of other services

---
+++ microservices/auth-service/server.ts
@@ -0,0 +1,17 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app: Application = express();
+
+
+const port = process.env.PORT || 5003;
+
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+
+    res.send('here we go: finance');
+})
+
+app.listen(port, () => {
+    console.log('app is listening on ', 8001);
+})
