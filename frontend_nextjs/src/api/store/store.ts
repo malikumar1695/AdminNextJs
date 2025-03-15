0f42d5794ad5dfb5e4b161eb8d7f1dce2d205a9a
Umer Sultan <malikumar1695@gmail.com>
2025-03-15 13:02:14 +0300

    redis-persist added

--- frontend_nextjs/src/api/store/store.ts
+++ frontend_nextjs/src/api/store/store.ts
@@ -1,14 +1,24 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 //import counterReducer from '../features/counter/counterSlice';
 import authReducer from '../store/authSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from "redux-persist/lib/storage"; // Default localStorage for web
+
+const persistConfig = {
+  key: "auth",
+  storage
+}
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
     //counter: counterReducer,
-    auth: authReducer
+    auth: persistedAuthReducer//authReducer
   },
 });
 
+export const persistor = persistStore(store);
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
--- frontend_nextjs/src/components/reactqueryprovider/ReactQueryProvider.tsx
+++ frontend_nextjs/src/components/reactqueryprovider/ReactQueryProvider.tsx
@@ -1,10 +1,11 @@
 
 'use client';
 
-import { store } from "@/api/store/store";
+import { persistor, store } from "@/api/store/store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 interface props {
     children: React.ReactNode
@@ -13,7 +14,7 @@ interface props {
 const ReactQueryProvider = ({ children }: props) => {
     const [queryClient] = useState(() => new QueryClient());
 
-    return <Provider store={store}> <QueryClientProvider client={queryClient}>{children}</QueryClientProvider></Provider>
+    return <Provider store={store}><PersistGate loading={null} persistor={persistor}> <QueryClientProvider client={queryClient}>{children}</QueryClientProvider></PersistGate></Provider>
 }
 
 export default ReactQueryProvider;
